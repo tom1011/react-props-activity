e3fe0633b43d2d9818e762325bf847e00628866e
David Friday <davidsfriday13@gmail.com>
2019-02-22 14:20:29 -0600

    enternumber up down working

--- src/components/EnterNumber/EnderNumber.js
+++ src/components/EnterNumber/EnderNumber.js
@@ -4,6 +4,34 @@ import CurrentTotal from '../CurrentTotal/CurrentTotal';
 class EnderNumber extends Component {
     state = {
         currentTotal: 0,
+        inputNumber: 0
+    }
+
+
+
+    changenumberUp = () => {
+        console.log('up botton clicked')
+        this.setState({
+            ...this.state,
+            currentTotal: Number(this.state.inputNumber) + Number(this.state.currentTotal),
+        })
+    }
+
+    changeNumberDown = () => {
+        console.log('up botton clicked')
+        this.setState({
+            ...this.state,
+            currentTotal:Number(this.state.currentTotal) - Number(this.state.inputNumber) ,
+        })
+    }
+
+
+
+    onChange = (event) => {
+        this.setState({
+            ...this.state,
+            inputNumber: event.target.value
+        })
     }
 
     render() {
@@ -11,9 +39,9 @@ class EnderNumber extends Component {
 
         return (
             <>
-                <button>Up</button>
-                <input />
-                <button>Down</button>
+                <button onClick={this.changenumberUp}>Up</button>
+                <input type="number" onChange={this.onChange}/>
+                <button onClick={this.changeNumberDown}>Down</button>
                 <CurrentTotal Total = {this.state.currentTotal} />
             </>
         );
